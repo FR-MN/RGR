e35c628f0377069d5dfc4b77f39ac61376803d5f
Ilya Bratchenko <bratchenko.ilya@gmail.com>
2017-03-28 23:09:00 +0400

    Исправление некоторых полей UserDTO

--- ArtAlbum/ArtAlbum.Entities/UserDTO.cs
+++ ArtAlbum/ArtAlbum.Entities/UserDTO.cs
@@ -9,15 +9,11 @@ namespace ArtAlbum.Entities
     public class UserDTO
     {
         public Guid Id { get; set; }
-
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
         public string Nickname { get; set; }
-
-        public string Mail { get; set; }
-        public string Password { get; set; }
-
+        public string Email { get; set; }
+        public int HashOfPassword { get; set; }
         public DateTime DateOfBirth { get; set; }
 
     }
