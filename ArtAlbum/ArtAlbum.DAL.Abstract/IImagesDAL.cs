d0b3c2a1369d20d073ba9ef626e44b1251b841af
Ilya Bratchenko <bratchenko.ilya@gmail.com>
2017-03-28 23:09:43 +0400

    Добавление интерфейсов DAL

---
+++ ArtAlbum/ArtAlbum.DAL.Abstract/IImagesDAL.cs
@@ -0,0 +1,18 @@
+﻿using ArtAlbum.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtAlbum.DAL.Abstract
+{
+    public interface IImagesDAL
+    {
+        bool AddImage(ImageDTO image);
+        UserDTO GetImageById(Guid imageId);
+        bool RemoveImageById(Guid imageId);
+        bool UpdateImage(ImageDTO image);
+        IEnumerable<ImageDTO> GetAllImages();
+    }
+}
