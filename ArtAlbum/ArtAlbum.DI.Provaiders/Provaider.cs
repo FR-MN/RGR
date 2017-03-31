c532545844599c8508c020d7973f7f2f6223038a
Ilya Bratchenko <bratchenko.ilya@gmail.com>
2017-03-31 19:57:53 +0400

    Закончены ImageDTO и UserDTO, создан Provaider

---
+++ ArtAlbum/ArtAlbum.DI.Provaiders/Provaider.cs
@@ -0,0 +1,53 @@
+﻿using ArtAlbum.BLL.Abstract;
+using ArtAlbum.BLL.DefaultLogic;
+using ArtAlbum.DAL.Abstract;
+using ArtAlbum.DAL.DataBase;
+using ArtAlbum.DAL.DataBase.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtAlbum.DI.Provaiders
+{
+    public static class Provaider
+    {
+        static Provaider()
+        {
+            string typeDAL = ConfigurationManager.AppSettings["DAL"];
+            string typeBLL = ConfigurationManager.AppSettings["BLL"];
+
+            switch (typeDAL)
+            {
+                case "DataBase":
+                    {
+                        UsersDAL = new UsersDAL();
+                        AwardsDAL = new AwardsDAL();
+                        RelationsDAL = new UsersAwardsDAL();
+                    }
+                    break;
+                default: { throw new ConfigurationFileException("error in configuration file"); }
+            }
+            switch (typeBLL)
+            {
+                case "DefaultLogic":
+                    {
+                        UsersBLL = new UsersBLL(UsersDAL, RelationsDAL);
+                        ImagesBLL = new ImagesBLL(ImagesDAL, RelationsDAL);
+                        RelationsBLL = new UsersImagesBLL(UsersDAL, ImagesDAL, RelationsDAL);
+                    }
+                    break;
+                default: { throw new ConfigurationFileException("error in configuration file"); }
+            }
+        }
+
+        public static IUsersDAL UsersDAL { get; private set; }
+        public static IImagesDAL ImagesDAL { get; private set; }
+        public static IUsersImagesDAL RelationsDAL { get; private set; }
+        public static IUserBLL UsersBLL { get; private set; }
+        public static IImageBLL ImagesBLL { get; private set; }
+        public static IUserImageBLL RelationsBLL { get; private set; }
+    }
+}
--- ArtAlbum/UsersImages.PL.Models/ImageVM.cs
+++ ArtAlbum/UsersImages.PL.Models/ImageVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ArtAlbum.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,59 @@ using System.Threading.Tasks;
 
 namespace UsersImages.PL.Models
 {
-    class ImageVM
+    public class ImageVM
     {
+        private string description;
+
+        public ImageVM(string description)
+        {
+            DateOfCreating = DateTime.Now;
+            Description = description;
+            Id = Guid.NewGuid();
+        }
+        public ImageVM(Guid id, string description, DateTime dateOfCreating)
+        {
+            DateOfCreating = DateTime.Now;
+            Description = description;
+            if (dateOfCreating > DateTime.Now || dateOfCreating.Year < 1960 || dateOfCreating == null)
+            {
+                throw new ArgumentNullException("date of creating is incorrect");
+            }
+            DateOfCreating = dateOfCreating;
+            if (id == null)
+            {
+                throw new ArgumentNullException("id is null");
+            }
+            Id = id;
+        }
+
+        public Guid Id { get; private set; }
+        public DateTime DateOfCreating { get; private set; }
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) && value.Length > 500)
+                {
+                    throw new ArgumentException("incorrect description");
+                }
+                description = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Description: {0}, date of creating: {1}", Description, DateOfCreating);
+        }
+
+        public static explicit operator ImageVM(ImageDTO data)
+        {
+            return new ImageVM(data.Id, data.Description, data.DateOfCreating);
+        }
+        public static implicit operator ImageDTO(ImageVM data)
+        {
+            return new ImageDTO() { Id = data.Id, Description = data.Description, DateOfCreating = data.DateOfCreating };
+        }
     }
 }
