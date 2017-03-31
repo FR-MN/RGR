0e180444c68a59915590b1aed0749301d29ca589
Ilya Bratchenko <bratchenko.ilya@gmail.com>
2017-03-31 22:01:54 +0400

    Восстановление?

---
+++ ArtAlbum/ArtAlbum.Entities/ImageDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtAlbum.Entities
+{
+    public class ImageDTO
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }           
+        public DateTime DateOfCreating { get; set; }
+    }
+}
