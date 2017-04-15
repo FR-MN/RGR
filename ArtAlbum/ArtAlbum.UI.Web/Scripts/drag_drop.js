7f74555c82bfb2d8a3764c77d351a1595ce9a9b0
Roman-PC\Roman <lezin.r@yandex.ru>
2017-04-15 21:27:04 +0400

    Загружены стили,и web страницы, Должно но провреить пока что не могу. Загрузку изображений оставил твою. С модной надо помучаться немного.

---
+++ ArtAlbum/ArtAlbum.UI.Web/Scripts/drag_drop.js
@@ -0,0 +1,155 @@
+﻿var addEvent = (function () {
+    if (document.addEventListener) {
+        return function (el, type, fn) {
+            if (el && el.nodeName || el === window) {
+                el.addEventListener(type, fn, false);
+            } else if (el && el.length) {
+                for (var i = 0; i < el.length; i++) {
+                    addEvent(el[i], type, fn);
+                }
+            }
+        };
+    } else {
+        return function (el, type, fn) {
+            if (el && el.nodeName || el === window) {
+                el.attachEvent('on' + type, function () { return fn.call(el, window.event); });
+            } else if (el && el.length) {
+                for (var i = 0; i < el.length; i++) {
+                    addEvent(el[i], type, fn);
+                }
+            }
+        };
+    }
+})();
+
+(function () {
+
+    var pre = document.createElement('pre');
+    pre.id = "view-source"
+
+    // private scope to avoid conflicts with demos
+    addEvent(window, 'click', function (event) {
+        if (event.target.hash == '#view-source') {
+            // event.preventDefault();
+            if (!document.getElementById('view-source')) {
+                // pre.innerHTML = ('<!DOCTYPE html>\n<html>\n' + document.documentElement.innerHTML + '\n</html>').replace(/[<>]/g, function (m) { return {'<':'&lt;','>':'&gt;'}[m]});
+                var xhr = new XMLHttpRequest();
+
+                // original source - rather than rendered source
+                xhr.onreadystatechange = function () {
+                    if (this.readyState == 4 && this.status == 200) {
+                        pre.innerHTML = this.responseText.replace(/[<>]/g, function (m) { return { '<': '&lt;', '>': '&gt;' }[m] });
+                        prettyPrint();
+                    }
+                };
+
+                document.body.appendChild(pre);
+                // really need to be sync? - I like to think so
+                xhr.open("GET", window.location, true);
+                xhr.send();
+            }
+            document.body.className = 'view-source';
+
+            var sourceTimer = setInterval(function () {
+                if (window.location.hash != '#view-source') {
+                    clearInterval(sourceTimer);
+                    document.body.className = '';
+                }
+            }, 200);
+        }
+    });
+
+})();
+
+var holder = document.getElementById('holder'),
+    tests = {
+        filereader: typeof FileReader != 'undefined',
+        dnd: 'draggable' in document.createElement('span'),
+        formdata: !!window.FormData,
+        progress: "upload" in new XMLHttpRequest
+    },
+    support = {
+        filereader: document.getElementById('filereader'),
+        formdata: document.getElementById('formdata'),
+        progress: document.getElementById('progress')
+    },
+    acceptedTypes = {
+        'image/png': true,
+        'image/jpeg': true,
+        'image/gif': true
+    },
+    progress = document.getElementById('uploadprogress'),
+    fileupload = document.getElementById('upload');
+
+"filereader formdata progress".split(' ').forEach(function (api) {
+    if (tests[api] === false) {
+        support[api].className = 'fail';
+    } else {
+        // FFS. I could have done el.hidden = true, but IE doesn't support
+        // hidden, so I tried to create a polyfill that would extend the
+        // Element.prototype, but then IE10 doesn't even give me access
+        // to the Element object. Brilliant.
+        support[api].className = 'hidden';
+    }
+});
+
+function previewfile(file) {
+    if (tests.filereader === true && acceptedTypes[file.type] === true) {
+        var reader = new FileReader();
+        reader.onload = function (event) {
+            var image = new Image();
+            image.src = event.target.result;
+            image.width = 250; // a fake resize
+            holder.appendChild(image);
+        };
+
+        reader.readAsDataURL(file);
+    } else {
+        holder.innerHTML += '<p>Uploaded ' + file.name + ' ' + (file.size ? (file.size / 1024 | 0) + 'K' : '');
+        console.log(file);
+    }
+}
+
+function readfiles(files) {
+    debugger;
+    var formData = tests.formdata ? new FormData() : null;
+    for (var i = 0; i < files.length; i++) {
+        if (tests.formdata) formData.append('file', files[i]);
+        previewfile(files[i]);
+    }
+
+    // now post a new XHR request
+    if (tests.formdata) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/devnull.php');
+        xhr.onload = function () {
+            progress.value = progress.innerHTML = 100;
+        };
+
+        if (tests.progress) {
+            xhr.upload.onprogress = function (event) {
+                if (event.lengthComputable) {
+                    var complete = (event.loaded / event.total * 100 | 0);
+                    progress.value = progress.innerHTML = complete;
+                }
+            }
+        }
+
+        xhr.send(formData);
+    }
+}
+
+if (tests.dnd) {
+    holder.ondragover = function () { this.className = 'hover'; return false; };
+    holder.ondragend = function () { this.className = ''; return false; };
+    holder.ondrop = function (e) {
+        this.className = '';
+        e.preventDefault();
+        readfiles(e.dataTransfer.files);
+    }
+} else {
+    fileupload.className = 'hidden';
+    fileupload.querySelector('input').onchange = function () {
+        readfiles(this.files);
+    };
+}
