f9d769c26bf154e985170aaa0d1a8fba22af58dc
Roman-PC\Roman <lezin.r@yandex.ru>
2017-04-17 12:44:17 +0400

    Добавлена регистрация, в RegisterController нет логики на добавление пользователя и проверки логина и ника, регистрация без стилей. стили потом подьедут.

---
+++ ArtAlbum/ArtAlbum.UI.Web/Controllers/RegisterController.cs
@@ -0,0 +1,41 @@
+﻿using ArtAlbum.UI.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArtAlbum.UI.Web.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Register(RegisterVM registerData)
+        {
+            if(registerData!=null)
+            {
+                if(ModelState.IsValid)
+                {
+                    //todo add user
+                }
+            }
+            return View(registerData);
+        }
+        public JsonResult CheckEmail(string Email)
+        {
+            var res = true;
+            return Json(res, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult CheckLogin(string Login)
+        {
+            var res = true;
+            return Json(res, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
