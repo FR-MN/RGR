ee5284f6c93f3c1930c9a4725a3521cd8719f29d
Ilya Bratchenko <bratchenko.ilya@gmail.com>
2017-04-18 16:03:25 +0400

    Пермещение ветки обратно в develop 2

---
+++ ArtAlbum/ArtAlbum.UI.Web/Models/RegisterVM.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArtAlbum.UI.Web.Models
+{
+    public class RegisterVM
+    {
+        [Required]
+        [Remote("CheckEmail","Register",ErrorMessage ="Такая почта уже есть")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [MinLength(3)]
+        [RegularExpression("^[a-zA-Z0-9][a-zA-Z0-9_-]+[a-zA-Z0-9]$")]
+        [Remote("CheckNickname","Register",ErrorMessage = "Такой Nickname уже есть")]
+        public string Nickname { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [System.ComponentModel.DataAnnotations.Compare("Password")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        [MinLength(3)]
+        [RegularExpression("^[a-zA-Zа-яА-Я]+((-[a-zA-Zа-яА-Я]+)+)|([a-zA-Zа-яА-Я]+)$")]
+        public string FirstName { get; set; }
+        [MinLength(3)]
+        [RegularExpression("^[a-zA-Zа-яА-Я]+((-[a-zA-Zа-яА-Я]+)+)|([a-zA-Zа-яА-Я]+)$")]
+        public string LastName { get; set; }                 
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ ArtAlbum/ArtAlbum.UI.Web/Scripts/dropdown_menu.js
@@ -0,0 +1,10 @@
+﻿
+var foo = $("#list_dropdowns");
+foo.ready(function () {
+
+    foo.find("ul").hide();
+
+    foo.find("h3 span").click(function () {
+        $(this).parent().next().slideToggle();
+    });
+});
