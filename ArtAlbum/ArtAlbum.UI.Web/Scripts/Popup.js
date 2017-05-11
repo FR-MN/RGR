cbd3e5534c210b034c479e7b7f3f52d1c7b0d332
Roman-PC\Roman <lezin.r@yandex.ru>
2017-05-11 21:40:17 +0400

    Добавлена загрузка изображения по нажатию на миниатюру.(работает ток с тем что в Partial'e)

--- ArtAlbum/ArtAlbum.UI.Web/Scripts/Popup.js
+++ ArtAlbum/ArtAlbum.UI.Web/Scripts/Popup.js
@@ -1,9 +1,25 @@
-﻿$(document).ready(function () {
-    PopUpHide();
-});
-function PopUpShow() {
-    $("#popup1").show();
-}
-function PopUpHide() {
-    $("#popup1").hide();
-}
+﻿(function () {
+    var $ref = $('.show-image'),
+    
+    img = new Image();
+    
+
+    $ref.on('click', function (e) {
+        
+        
+
+        var imageid = $(this).attr("data-target").slice(1),
+            temp = "#" + imageid,
+            md = $(temp).find($(".top_image"));
+            $(temp).modal()
+        e.preventDefault();
+        
+        
+        img.src = '/Images/GetImage/' + imageid;
+        md.append(img);
+
+    })
+       
+     
+
+    })();
